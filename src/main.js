c49d72e6f1697ea22c7208cd48e3b0a64cde3726
sanguk <sdag3692@naver.com>
2022-08-01 15:46:58 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function buttonClick(nsp){
+    const socket = io("http://localhost:3000/namespace"+nsp);
+
+    socket.on("text",(text)=>{
+        console.log(text);
+        console.log(socket.id);
+    })
+
+}
